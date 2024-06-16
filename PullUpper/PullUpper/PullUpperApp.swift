037966b9c0b62a864b35d75e23e1f234cb4a3693
seoyounghan <happyunbd364@naver.com>
2024-06-16 16:27:26 +0900

    first commit

---
+++ PullUpper/PullUpper/PullUpperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullUpperApp.swift
+//  PullUpper
+//
+//  Created by hanseoyoung on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PullUpperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
