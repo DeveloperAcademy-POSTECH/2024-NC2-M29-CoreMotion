cff6f600a26ec8095844a17a7449caa6814d7eb7
seoyounghan <happyunbd364@naver.com>
2024-06-18 00:46:58 +0900

    [#3] MainView 뷰 그리기 & 유저 디폴트 적용

--- PullUpper/PullUpper/View/MainView.swift
+++ PullUpper/PullUpper/View/MainView.swift
@@ -8,12 +8,53 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var userGoal: Int = UserDefaults.standard.integer(forKey: "userGoal")
+
+    let goalNumbers: [Int] = Array(1...20)
+
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             Text("PullUpper")
-                .font(Font.custom("Stretch Pro", size: 36))
+                .font(Font.custom("StretchProRegular", size: 36))
                 .multilineTextAlignment(.center)
                 .foregroundColor(.black)
+                .padding(.top, 80)
+
+            Text("GOAL")
+                .font(.system(size: 42))
+                .foregroundColor(.subText)
+                .padding(.top, 81)
+
+            Menu {
+                Picker(selection: $userGoal) {
+                    ForEach(goalNumbers, id: \.self) { goalNumber in
+                        Text("\(goalNumber)")
+                    }
+                } label: { }
+            } label: {
+                Text("\(userGoal)")
+                    .font(.system(size: 96))
+                    .foregroundColor(.mainText)
+                    .underline()
+            }
+
+            Button(action: {
+                UserDefaults.standard.set(self.userGoal, forKey: "userGoal")
+            }, label: {
+                ZStack {
+                    Image("Start_Button")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+
+                    Text("START")
+                        .font(.system(size: 32))
+                        .foregroundColor(.mainText)
+                }
+            })
+            .padding(.top, 89)
+            .padding(.horizontal, 77)
+
+            Spacer()
         }
     }
 }
