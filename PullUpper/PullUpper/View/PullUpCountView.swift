5d856da0d1c96b627511fd3c3deae7fa24211233
Byhoon kim (Miles) <by0881@naver.com>
2024-06-18 09:34:11 +0900

    #4 PullUpCountView 컬러세팅
    github desktop 이슈 해결 중..

--- PullUpper/PullUpper/View/PullUpCountView.swift
+++ PullUpper/PullUpper/View/PullUpCountView.swift
@@ -7,34 +7,72 @@
 
 import SwiftUI
 
-
 struct PullUpCountView: View {
+    
+    
+    
     var body: some View {
-        VStack{
-            Spacer()
+        ZStack{
+            ZStack{
+                Color.accentColor
+                    .ignoresSafeArea()
+                Image("white half 1")
+                    .resizable()
+                    .opacity(0.5)
+            }
             VStack{
-                Text("COUNT")
-                //숫자는 pullUpCount
-                //            Text("\(pullUpCount)")
-                Text("11")
-            }
-            Spacer()
-            HStack{
-                VStack{
-                    Text("GOAL")
-                    Divider()
-                    HStack{
-                        Text("20")
-                        Text("PULL UPs")
+                Spacer()
+                VStack(spacing: 0){
+                    Text("COUNT")
+                        .font(.system(size: 40, weight: .heavy))
+                        .fontWidth(.expanded)
+                        .foregroundStyle(.white)
+                        .opacity(0.5)
+                    //숫자는 pullUpCount
+                    Text("11")
+                        .font(.system(size: 128, weight: .heavy))
+                        .fontWidth(.expanded)
+                        .foregroundStyle(.white)
+                }
+                Spacer()
+                HStack{
+                    VStack(alignment: .leading, spacing: 8){
+                        Text("GOAL")
+                            .font(.system(size: 24, weight: .bold))
+                            .foregroundStyle(.subText)
+                        
+                        Divider()
+                        HStack{
+                            Text("20")
+                                .font(.system(size: 48, weight: .black))
+                                .fontWidth(.expanded)
+                            Text("PULL\nUPs")
+                                .font(.system(size: 16, weight: .bold))
+                                .fontWidth(.expanded)
+                        }
+                        
                     }
-                    
+                    VStack(alignment: .leading, spacing: 8){
+                        Text("TIME")
+                            .font(.system(size: 24, weight: .bold))
+                            .foregroundStyle(.subText)
+                        Divider()
+                        Text("00:00")
+                            .font(.system(size: 48, weight: .black))
+                    }
                 }
-                VStack{
-                    Text("GOAL")
-                    Divider()
-                    Text("00:00")
-                }
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(.black)
+                            .frame(height: 66)
+                        Text("STOP")
+                            .font(.system(size: 36, weight: .black))
+                            .fontWidth(.expanded)
+                    }
+                })
             }
+            .padding()
         }
     }
 }
